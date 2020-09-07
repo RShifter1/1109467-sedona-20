e32eb3e787df8036a537447a25c9c3107fc62ce3
Mark <alisermzark@gmail.com>
2020-09-07 16:03:34 +0300

    automatization

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,53 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+// const del = require("gulp-del");
+const svgstore = require("gulp-svgstore")
+
+
+//build
+
+const build = () => gulp.series (
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+);
+
+//start
+
+const start = () => gulp.series (
+  "build",
+  "server"
+);
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("source/img"))
+}
+
+
+// WebP
+
+const webpicture = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("source/img"))
+}
+
+exports.webp = webpicture;
 
 // Styles
 
@@ -16,12 +63,54 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+
+// sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+  .pipe(svgstore())
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("source/img"))
+}
+
+exports.sprite = sprite;
+
+// delete
+
+const clean = () => {
+  return del ("build");
+}
+
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/*",
+    "source/js/*.js",
+    "source/*.ico"
+
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+
+
+
 
 // Server
 
@@ -49,3 +138,6 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// gulp.task("build", gulp.series("styles, "));
+// gulp.task("start" gulp.series("build, server"));
